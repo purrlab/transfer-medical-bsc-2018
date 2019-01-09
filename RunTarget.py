ffa90abf097357226861b0f860972490423843cd
FlorisFok <43857896+FlorisFok@users.noreply.github.com>
2019-01-09 12:03:32 +0100

    labby

--- RunTarget.py
+++ RunTarget.py
@@ -24,8 +24,8 @@ def run_target(params):
     model = make_model(x, y, params)
 
     if params["style"] == 'FT':
-        weights = determen_weights(y)
-        H, score, model = train_model(model,x,y,x_val,y_val,x_test,y_test, params, weights)
+        # weights = determen_weights(y)
+        H, score, model = train_model(model,x,y,x_val,y_val,x_test,y_test, params)#, weights)
         predictions = get_feature_vector(model, x, layer = 'fc2')
         predictions_test = get_feature_vector(model, x_test, layer = 'fc2')
         score = auc_svm(predictions,y,predictions_test,y_test, plot = False)
