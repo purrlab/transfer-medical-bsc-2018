12609fece41ce81d8b965da37acfa27d00ddd8a1
FlorisFok <43857896+FlorisFok@users.noreply.github.com>
2019-02-21 11:58:26 +0100

    deletion

--- BloodRun.bat
+++
@@ -1,7 +0,0 @@
-MODE CON: COLS=200 LINES=50
-
-python run1.py 2002
-python run1.py 2102
-python run1.py 2202
-python run1.py 2302
-python run1.py 2402
--- FAILED_RUNAGAIN.bat
+++
@@ -1,7 +0,0 @@
-MODE CON: COLS=200 LINES=50
-python run1.py 0207
-python run1.py 0205
-
-
-
-
